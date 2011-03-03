1333abeaab1f9d73fd3ed225b750885966b7dd91
phimuemue <contact@phimuemue.com>
2011-03-03 11:02:31 +0100

    Added c-helpers-minor-mode

---
+++ c-helpers-minor-mode/c-helpers.el
@@ -0,0 +1,18 @@
+(define-minor-mode c-helpers-minor-mode
+  "This mode contains little helpers for C developement"
+  nil
+  ""
+  '(((kbd "{") . insert-c-block-parentheses))
+)
+
+(defun insert-c-block-parentheses ()
+  (interactive)
+  (insert "{")
+  (newline)
+  (newline)
+  (insert "}")
+  (indent-for-tab-command)
+  (previous-line)
+  (indent-for-tab-command)
+  )
+
